65245d484c2f4d4af2a7c9035d4e853077ca04bb
Vasily <vasa-svistun@mail.ru>
2020-11-05 09:48:03 +0300

    Первый проект(дополненый)

--- проект 1/main.js
+++ проект 1/main.js
@@ -1,17 +1,43 @@
+"use strict";
 
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",'');
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDb = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat : false,
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 };
 
+for (let i = 0; i < 2; i++) {
+  let questionOne = prompt("Один из последних просмотренных фильмов", ""),
+    questionOneResult = prompt("На сколько оцените его?", "");
 
-let questionOne = prompt('Один из последних просмотренных фильмов',''),
-questionOneResult =  prompt('На сколько оцените его?','');	
+  if (
+    questionOne != null &&
+    questionOneResult != null &&
+    questionOne != "" &&
+    questionOneResult != "" &&
+    questionOne.length < 50 &&
+    questionOneResult.length < 50
+  ) {
+    personalMovieDb.movies[questionOne] = questionOneResult;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
 
+if (personalMovieDb.count < 10) {
+  alert("Просмотренно слишком мало фильмов");
+} else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
+  alert("Вы классический зритель");
+} else if (personalMovieDb.count >= 30) {
+  alert("Вы киноман!");
+} else {
+  alert("Произошла ошибка!");
+}
 
-personalMovieDb.movies[questionOne] = questionOneResult;
+console.log(personalMovieDb);
